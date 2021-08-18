4b6fd39e6f4b973d797b9b8ccda4f9075e234b9c
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-17 21:26:53 -0400

    setting up an interface to accompany API

---
+++ Item.cs
@@ -0,0 +1,21 @@
+namespace ApiClient
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
+
+
+
+
+
+        //sample data
+        //"id": 278,
+        //"type": "general",
+        //"setup": "What's black and white and read all over?",
+        //"punchline": "The newspaper."
+
+    }
+}
