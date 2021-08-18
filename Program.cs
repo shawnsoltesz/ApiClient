c473772ed77d3886ab754ca50062fbe4117b3f62
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-17 22:13:23 -0400

    updated Greeting

--- Program.cs
+++ Program.cs
@@ -1,28 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace ApiClient
 {
     class Program
     {
 
-
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
-            var responseBodyAsString = await client.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+            var todayDate = DateTime.Now;
 
-            Console.WriteLine(responseBodyAsString);
+            //var responseBodyAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_joke");
 
+            //var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsStream);
 
+            var responseBodyAsStream10 = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_ten");
 
-            var responseBodyAsString10 = await client.GetStringAsync("https://official-joke-api.appspot.com/random_ten");
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsStream10);
+            //var table = new ConsoleTable("Joke", "Punchline");
+            Console.WriteLine("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+            Console.WriteLine("                            Jokes of the Day\n");
+            Console.WriteLine($"                     Today is {todayDate}\n");
+            Console.WriteLine("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
 
-            Console.WriteLine(responseBodyAsString10);
-
-
+            foreach (var item in items)
+            {
+                //table.AddRow(item.Setup, item.Punchline);
+                Console.WriteLine($"{item.Setup}\n");
+                Console.WriteLine($"Punchline: {item.Punchline}\n\n");
+            }
+            //table.Write();
         }
     }
 }
