f52d2a76635455cc7aa57fbc1c3252a6c79934bb
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-17 21:03:39 -0400

    Item class

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
+
+            var responseBodyAsString = await client.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+
+            Console.WriteLine(responseBodyAsString);
+
+
+
+            var responseBodyAsString10 = await client.GetStringAsync("https://official-joke-api.appspot.com/random_ten");
+
+            Console.WriteLine(responseBodyAsString10);
+
+
         }
     }
 }
